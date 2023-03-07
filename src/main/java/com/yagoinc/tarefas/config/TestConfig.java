4b6ae6d5ea5aa13efcf3def4a56953986c1573cd
Yago Alexandre <goltarayago@gmail.com>
2023-03-07 12:58:19 -0300

    FindAll and FindById - Users

--- src/main/java/com/yagoinc/tarefas/config/TestConfig.java
+++ src/main/java/com/yagoinc/tarefas/config/TestConfig.java
@@ -4,10 +4,15 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.yagoinc.tarefas.entities.User;
 import com.yagoinc.tarefas.repository.UserRepository;
 
+
+@Configuration
+@Profile("test")
 public class TestConfig implements CommandLineRunner {
 
 	@Autowired
